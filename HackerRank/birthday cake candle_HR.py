c9f3037b36a50d38ce85c645bcf48eeff52ccc31
Ashutosh Kumar Shubham <109526834+ashutoshshubham@users.noreply.github.com>
2022-09-03 18:07:32 +0530

    Delete HackerRank directory

--- HackerRank/birthday cake candle_HR.py
+++
@@ -1,7 +0,0 @@
-def birthdayCakeCandles(candles):
-    maximum = max(candles)
-    count = candles.count(maximum)
-    print(count)
-
-candles = list(map(int,input("Enter  Size of candles ").split()))
-birthdayCakeCandles(candles)
