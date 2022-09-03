d85b49bc47652f71622664a3ab4a32c6bf6127c4
Ashutosh Kumar Shubham <109526834+ashutoshshubham@users.noreply.github.com>
2022-09-03 18:17:13 +0530

    practice

--- HackerRank/string split and join_HR.py
+++
@@ -1,9 +0,0 @@
-def split_and_join(line):
-    line = line.split(" ")
-    line = "-".join(line)
-    return (line)
-
-if __name__ == '__main__':
-    line = input("Enter string\n")
-    result = split_and_join(line)
-    print(result)
